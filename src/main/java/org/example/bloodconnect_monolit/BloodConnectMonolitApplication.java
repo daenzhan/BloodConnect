4b697996c645b4d9563e6b4128d8e5dcbbb4b461
Arusha <storio.tommy@gmail.com>
2026-02-22 18:10:04 +0500

    created frontend folder with library

---
+++ src/main/java/org/example/bloodconnect_monolit/BloodConnectMonolitApplication.java
@@ -0,0 +1,13 @@
+package org.example.bloodconnect_monolit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodConnectMonolitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloodConnectMonolitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/bloodconnect_monolit/BloodConnectMonolitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bloodconnect_monolit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodConnectMonolitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
