31eaf6c9e7d50823d9bf1bd00f301934a3c78927
ttmira <tamara.kazybaeva2005@gmail.com>
2026-05-11 06:36:08 +0500

    update auth, fix sidebars, improve medical center dashboard and components

--- frontend/src/app/auth/auth-api.ts
+++ frontend/src/app/auth/auth-api.ts
@@ -237,11 +237,8 @@ function transformToBackendFormat(frontendData: any): any {
     console.log('Frontend data received in transform:', frontendData);
     console.log('Confirm password value:', frontendData.confirmPassword);
 
-<<<<<<< HEAD
-    const { email, password,confirmPassword, phoneNumber, role, donorData, bloodCenterData, medicalCenterData } = frontendData;
-=======
     const { email, password, confirmPassword, phoneNumber, role, donorData, bloodCenterData, medicalCenterData } = frontendData;
->>>>>>> 0665e7dac327bb19c8502f460484341b93da4696
+
 
     const backendData: any = {
         email: email || '',
@@ -284,10 +281,6 @@ function transformToBackendFormat(frontendData: any): any {
         Object.assign(backendData, {
             medCenterName: medicalCenterData.medCenterName || '',
             medCenterLocation: medicalCenterData.location || '',
-<<<<<<< HEAD
-=======
-            medCenterPhone: medicalCenterData.phone || '',
->>>>>>> 0665e7dac327bb19c8502f460484341b93da4696
             medCenterSpecialization: medicalCenterData.specialization || '',
             medCenterDirectorFullName: medicalCenterData.directorFullName || '',
         });
